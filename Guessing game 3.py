5e4f4fe24b3b4ef856be4c35c78813a491d9df39
michaldoman <mjd.domanski@gmail.com>
2022-12-08 22:48:18 +0100

    almost working version

--- Guessing game 3.py
+++ Guessing game 3.py
@@ -11,28 +11,26 @@ def main():
     attempt = 1
     invitation_str = f'Computer: Think of a number between {minimum} and {maximum}, ' \
                      f'and I will try to guess it in {attempts} tries!'
-    guess_str = ''
 
     if request.method == 'POST':
-        while attempt <= attempts:
-            guess = int((maximum - minimum) / 2) + minimum
-            guess_str = f'Computer: My guess number {attempt}, is "{guess}"'
+        minimum = int(request.form.get('minimum', 0))
+        maximum = int(request.form.get('maximum', 1000))
+        guess = int(request.form.get('guess', 500))
+        guess_str = f'Computer: My guess number {attempt}, is "{guess}"'
+        user_info = request.form.get("info")
 
-            user_info = request.form.get("info", None)
-            if user_info == 'correct':
-                guess_str = 'Computer: Yeah! I am always the best!'
-                break
-            elif user_info == 'too small':
-                minimum = guess
-                attempt += 1
-            elif user_info == 'too big':
-                maximum = guess
-                attempt += 1
-            else:
-                pass
+        if user_info == 'correct':
+            guess_str = 'Computer: Yeah! I am always the best!'
+        elif user_info == 'too small':
+            minimum = guess
+            attempt += 1
+        elif user_info == 'too big':
+            maximum = guess
+            attempt += 1
+        guess = int((maximum - minimum) / 2) + minimum
 
-            return render_template('Guessing game 3.html', mtd='POST', guess=guess_str)
-        return render_template('Guessing game 3.html', mtd='POST', guess=guess_str)
+        return render_template('Guessing game 3.html', mtd='POST', guess=guess, guess_str=guess_str)
+
     return render_template('Guessing game 3.html', invitation=invitation_str, mtd='GET')
 
 
